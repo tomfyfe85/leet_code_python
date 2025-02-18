7a7047bbc6a7fe799b8b18c58c8df31c98065ce8
tomfyfe <tomfyfe85@gmail.com>
2025-02-18 13:03:31 +0000

    merge_sorted_arrays

---
+++ leet_code/merge_sorted_array.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+	"""
+	Do not return anything, modify nums1 in-place instead.
+	"""
+	# p1 = last initialised num of nums1
+	# len(nums1) - n
+	# p2 = last num of nums2
+	# nums.pop()
+
+	# p3 = 0 after p1
+	# p1 + 1
+	# if p2 > p1
+	# nums[-1] = p2. - loop [::-1]
+
+
+
+
