2ca37ed0729cdadc9c6aed92adc579344077baec
tomfyfe <tomfyfe85@gmail.com>
2025-02-18 01:40:36 +0000

    complete

--- leet_code/two_sum.py
+++ leet_code/two_sum.py
@@ -1,1 +1,13 @@
+from typing import List
+
+
 def twoSum(self, nums: List[int], target: int) -> List[int]:
+	hash_map = {i: num for i, num in zip(nums, range(0, len(nums)))}
+
+	next_num = 1
+	for i, num in enumerate(nums):
+		comp = target - num
+		if comp in nums[next_num:]:
+			return [i, hash_map[comp]]
+		next_num += 1
+
